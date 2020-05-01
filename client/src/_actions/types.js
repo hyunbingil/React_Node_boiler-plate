e77b9428aa273978f81510f407c50e71ac0a44c4
hyunbingil <super199g@naver.com>
2020-05-01 16:11:51 +0900

    React.Node: 끝

--- client/src/_actions/types.js
+++ client/src/_actions/types.js
@@ -1,3 +1,4 @@
 // 모든 type은 여기에서 지정한다.
 export const LOGIN_USER = "login_user";
 export const REGISTER_USER = "register_user";
+export const AUTH_USER = "auth_user";
--- client/src/_actions/user_action.js
+++ client/src/_actions/user_action.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
-import { LOGIN_USER } from './types'
-import { REGISTER_USER } from './types'
+import { LOGIN_USER, REGISTER_USER, AUTH_USER } from './types'
 
 export function loginUser(dataTosubmit) {
     const request = axios.post('/api/users/login', dataTosubmit)
@@ -18,6 +17,16 @@ export function registerUser(dataTosubmit) {
 
     return {
         type: REGISTER_USER,
+        payload: request
+    }
+}
+
+export function auth() {
+    const request = axios.get('/api/users/auth')
+        .then(response => response.data) // 서버로 보내기
+
+    return {
+        type: AUTH_USER,
         payload: request
     }
 }
